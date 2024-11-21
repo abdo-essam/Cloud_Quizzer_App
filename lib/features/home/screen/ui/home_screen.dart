9e078c501702ab99a39503cc6898335cef3aab57
abdo <abdo.e.mosa@gmail.com>
2024-11-21 17:54:53 +0200

    Refactor(ui): Improve HomeScreen bottom navigation
    Refactored the bottom navigation bar in HomeScreen for better
    readability and maintainability.  Extracted common styles and
    padding into variables, and simplified the GButton configurations.

--- lib/features/home/screen/ui/home_screen.dart
+++ lib/features/home/screen/ui/home_screen.dart
@@ -17,7 +17,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
-  void updateCurrentIndex(int index) {
+  // List of screens for navigation
+  final List<Widget> pages = [
+    const HomeScreenBody(),
+    const HistoryScreen(),
+    const BookmarkScreen(),
+  ];
+
+  void _updateCurrentIndex(int index) {
     setState(() {
       _selectedIndex = index;
     });
@@ -27,60 +34,58 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[_selectedIndex],
-      bottomNavigationBar: Container(
-        color: ColorManager.black,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8),
-            child: GNav(
-              curve: Curves.easeOutExpo,
-              rippleColor: ColorManager.white.withOpacity(0.3),
-              hoverColor: ColorManager.white.withOpacity(0.1),
-              haptic: true,
-              tabBorderRadius: 20,
-              gap: 5,
-              activeColor: Colors.white,
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              duration: Duration(milliseconds: 400),
-              tabBackgroundColor: ColorManager.primaryColor.withOpacity(0.7),
-              textStyle: GoogleFonts.lato(
-                color: ColorManager.white,
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    // Common padding and styles
+    const double iconSize = 28.0;
+    final TextStyle textStyle = GoogleFonts.lato(color: ColorManager.white);
+    final EdgeInsets padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 12);
+
+    return Container(
+      color: ColorManager.black,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          child: GNav(
+            curve: Curves.easeOutExpo,
+            rippleColor: ColorManager.white.withOpacity(0.3),
+            hoverColor: ColorManager.white.withOpacity(0.1),
+            haptic: true,
+            tabBorderRadius: 20,
+            gap: 5,
+            activeColor: Colors.white,
+            padding: padding,
+            duration: const Duration(milliseconds: 400),
+            tabBackgroundColor: ColorManager.primaryColor.withOpacity(0.7),
+            textStyle: textStyle,
+            tabs: [
+              GButton(
+                iconSize: iconSize,
+                icon: _selectedIndex == 0 ? Icons.home : Icons.home_outlined,
+                iconColor: _selectedIndex == 0 ? ColorManager.white : ColorManager.grayX,
+                text: 'Home',
               ),
-              tabs: [
-                GButton(
-                  iconSize: _selectedIndex != 0 ? 28 : 25,
-                  icon: _selectedIndex == 0 ? Icons.home : Icons.home_outlined,
-                  iconColor: _selectedIndex == 0 ? ColorManager.white : ColorManager.grayX,
-                  text: 'Home',
-                ),
-                GButton(
-                  icon: _selectedIndex == 1
-                      ? Icons.history
-                      : Icons.history_outlined,
-                  iconColor: _selectedIndex == 0 ? ColorManager.white : ColorManager.grayX,
-                  text: 'History Score',
-                ),
-                GButton(
-                  iconSize: 28,
-                  icon: _selectedIndex == 2
-                      ? Icons.bookmark
-                      : Icons.bookmark_outline,
-                  iconColor: _selectedIndex == 0 ? ColorManager.white : ColorManager.grayX,
-                  text: 'Bookmark',
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: updateCurrentIndex,
-            ),
+              GButton(
+                iconSize: iconSize,
+                icon: _selectedIndex == 1 ? Icons.history : Icons.history_outlined,
+                iconColor: _selectedIndex == 1 ? ColorManager.white : ColorManager.grayX,
+                text: 'History Score',
+              ),
+              GButton(
+                iconSize: iconSize,
+                icon: _selectedIndex == 2 ? Icons.bookmark : Icons.bookmark_outline,
+                iconColor: _selectedIndex == 2 ? ColorManager.white : ColorManager.grayX,
+                text: 'Bookmark',
+              ),
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: _updateCurrentIndex,
           ),
         ),
       ),
     );
   }
-
-  final pages = [
-    HomeScreenBody(),
-    HistoryScreen(),
-    BookmarkScreen(),
-  ];
 }
